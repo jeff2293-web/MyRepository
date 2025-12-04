88fa1b7282be5095dea02e80732e6ea2e5b1183d
admin <admin@DESKTOP-I6HUGBR>
2025-12-04 14:27:05 +0900

    Change in master branch

--- GitHW/src/mygit/Yourclass.java
+++ GitHW/src/mygit/Yourclass.java
@@ -5,6 +5,7 @@ public class Yourclass {
 	public void sayHello(String toWhom) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello " + toWhom);
+		System.out.println("Your changed in master branch");
 	}
 
 }
